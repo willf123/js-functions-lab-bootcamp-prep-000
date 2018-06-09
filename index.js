ca7a84e210429a22fb8eed1659cccf35a5362947
willf123@yahoo.com <willf123@yahoo.com>
2018-06-09 15:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `"Happy holidays, ${name}!"`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidaysTo(holiday, name) {
-  return `"Happy ${holiday}, ${name}!"`;
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(days, holiday) {
-  return `"It's ${days} days until ${holiday}!"`;
+  return `It's ${days} days until ${holiday}!`;
 }
